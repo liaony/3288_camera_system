fa3cebf0a4c87f29dfda113d7b82a2f2e1cc1868
liaony <360096844@qq.com>
2018-12-10 18:01:31 +0800

    Initial commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 13 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#13 SMP PREEMPT Mon Oct 8 16:52:38 HKT 2018"
+#define LINUX_COMPILE_BY "liaony"
+#define LINUX_COMPILE_HOST "ubuntu092"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
